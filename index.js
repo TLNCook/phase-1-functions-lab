b8f264b2167875fb1e3087ce58ec771584e66854
Thomas Cook <tlncook85@gmail.com>
2022-05-16 22:21:30 -0600

    commit tree 2cff82b6a10d95ebd56550826cdc140a7cd60a07

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const hqStartingBlock = 42;
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - hqStartingBlock);    
+}
+function distanceFromHqInFeet(block) {
+    return Math.abs(distanceFromHqInBlocks(block) * 264);
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start)) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    if ((Math.abs((destination - start)) * 264) <= 400) {
+        return 0;
+    } else if ((Math.abs((destination - start)) * 264) > 400 && (Math.abs((destination - start)) * 264) <= 2000) {
+        return (((Math.abs((destination - start)) * 264) - 400) * 2) / 100;
+    } else if ((Math.abs((destination - start)) * 264) > 2000 && (Math.abs((destination - start)) * 264) < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
